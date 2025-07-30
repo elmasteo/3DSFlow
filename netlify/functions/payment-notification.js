ad2d2c058d3ce26a6b3e34808f4c2422d490c33d
unknown <sago980302@hotmail.com>
2025-07-30 09:39:39 -0500

    ajuste funcion de payment notification

--- netlify/functions/payment-notification.js
+++ netlify/functions/payment-notification.js
@@ -1,35 +1,29 @@
 const querystring = require('querystring');
 
 exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: 'Método no permitido. Usa POST.'
-    };
-  }
-
-  const contentType = event.headers['content-type'] || event.headers['Content-Type'];
-
-  // Validamos que sea tipo x-www-form-urlencoded
-  if (!contentType.includes('application/x-www-form-urlencoded')) {
-    return {
-      statusCode: 400,
-      body: 'Content-Type no soportado. Esperado: application/x-www-form-urlencoded.'
-    };
-  }
-
-  // Parsear el cuerpo de la solicitud
-  const parsedData = querystring.parse(event.body);
-
-  console.log("Notificación recibida desde Nuvei:", parsedData);
-
-  // Convertimos a query string para redirigir a la página de visualización
-  const queryString = new URLSearchParams(parsedData).toString();
-
-  return {
-    statusCode: 302,
-    headers: {
-      Location: `/notificacion.html?${queryString}`
+  if (event.httpMethod === 'POST') {
+    const contentType = event.headers['content-type'] || event.headers['Content-Type'];
+    if (!contentType.includes('application/x-www-form-urlencoded')) {
+      return {
+        statusCode: 400,
+        body: 'Content-Type no soportado. Esperado: application/x-www-form-urlencoded.'
+      };
     }
+
+    const parsedData = querystring.parse(event.body);
+    const queryString = new URLSearchParams(parsedData).toString();
+
+    return {
+      statusCode: 302,
+      headers: {
+        Location: `/notificacion.html?${queryString}`
+      }
+    };
+  }
+
+  // GET manual directo sin datos (no redirigido desde POST)
+  return {
+    statusCode: 400,
+    body: 'Esta función espera recibir un POST desde Nuvei.'
   };
 };
